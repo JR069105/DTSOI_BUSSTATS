44cf3655be65cf2b2fc0cbf89d5293b80d5a6ca9
JR069105 <74625492+JR069105@users.noreply.github.com>
2021-11-03 20:04:14 -0500

    CSS#7

--- Scraper.py
+++ Scraper.py
@@ -33,7 +33,7 @@ def web_gen():
         with div():
             img(src="SOIBus.png", _class="timage")
         with div():
-            h1("DTSOI Buses Status", _class = "title")
+            h1("DTSOI Bus Status", _class = "title")
             hr(_class = "line")
             p("Last Updated: ",(datetime.now(pytz.timezone("America/Chicago")).strftime("%a %b %d %I:%M %p, %Y ")),_class = "subtitle")
             for school in allb.keys():
