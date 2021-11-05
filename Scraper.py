62282e7f8534d1123745b4edd1bbd437c406976c
Joseph Rouse <74625492+JR069105@users.noreply.github.com>
2021-11-05 12:01:52 -0500

    #2 change numobus

--- Scraper.py
+++ Scraper.py
@@ -44,7 +44,7 @@ def web_gen():
             h1("DTSOI Bus Status", _class = "title")
             hr(_class = "line")
             p("Last Updated: ",(datetime.now(pytz.timezone("America/Chicago")).strftime("%a %b %d %I:%M %p, %Y ")),_class = "subtitle")
-            for i in full_busnums:
+            for i in full_buses:
                 numobus+=1
             if numobus == 0:
                 p("|")
